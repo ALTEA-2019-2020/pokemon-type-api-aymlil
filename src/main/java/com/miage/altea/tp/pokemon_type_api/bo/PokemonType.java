215166dedd3e67a2cce8e3c7ead6d32a666d989a
Antonelli Aymerick <antonelli@b13p11.fil.univ-lille1.fr>
2020-01-20 15:15:32 +0100

    Commit du jour, bonjour

---
+++ src/main/java/com/miage/altea/tp/pokemon_type_api/bo/PokemonType.java
@@ -0,0 +1,14 @@
+package com.miage.altea.tp.pokemon_type_api.bo;
+
+import java.util.List;
+
+public class PokemonType {
+    private int id;
+    private int baseExperience;
+    private int height;
+    private String name;
+    private Sprites sprites;
+    private Stats stats;
+    private int weight;
+    private List<String> types;
+}
---
+++ src/main/java/com/miage/altea/tp/pokemon_type_api/repository/PokemonTypeRepositoryImpl.java
@@ -0,0 +1,43 @@
+package com.miage.altea.tp.pokemon_type_api.repository;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.miage.altea.tp.pokemon_type_api.bo.PokemonType;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class PokemonTypeRepositoryImpl {
+    private List<PokemonType> pokemons;
+
+    public PokemonTypeRepositoryImpl() {
+        try {
+            var pokemonsStream = this.getClass().getResourceAsStream("/pokemons.json");
+
+            var objectMapper = new ObjectMapper();
+            var pokemonsArray = objectMapper.readValue(pokemonsStream, PokemonType[].class);
+            this.pokemons = Arrays.asList(pokemonsArray);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public PokemonType findPokemonTypeById(int id) {
+        System.out.println("Loading Pokemon information for Pokemon id " + id);
+
+        // TODO
+    }
+
+    @Override
+    public PokemonType findPokemonTypeByName(String name) {
+        System.out.println("Loading Pokemon information for Pokemon name " + name);
+
+        // TODO
+    }
+
+    @Override
+    public List<PokemonType> findAllPokemonType() {
+        // TODO
+    }
+}
